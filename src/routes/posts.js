8456a944fa17c7a2223dbcb327b21f8c1ee07de1
anettemarjaana <51910985+anettemarjaana@users.noreply.github.com>
2020-12-12 13:03:36 +0200

    Base is ready Editing posts function works. Now next step is adding support for multiple users. This means a new schema in the database models and log in functions.

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -11,6 +11,14 @@ router.get("/new", (req, res) => {
   });
 });
 
+/* The Edit Post -page will be in an address ".../posts/edit". */
+router.get("/edit/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("posts/edit", {
+    post: post
+  });
+});
+
 /* Once the post is ready, the app will redirect the user to see the new post.
 That view model is feed: */
 router.get("/:urlSlug", async (req, res) => {
@@ -26,32 +34,26 @@ router.get("/:urlSlug", async (req, res) => {
 });
 
 /* Hitting the save button on posts/new will call this function.
-The function should save the post in the database. */
+It creates a new post and redirects to savePost function. */
 
-router.post("/", async (req, res) => {
-  let post = new Post({
-    /* Fetch the information of the post from the _formfields
-    and save them in database */
-    title: req.body.title,
-    content: req.body.content
-  });
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.post = new Post();
+    next();
+  },
+  savePost("new")
+);
 
-  try {
-    /* If there's a success: */
-    /* Wait till the post is saved in database and then redirect to page /posts/id */
-    post = await post.save();
-    console.log("Inserted 1 post");
-    res.redirect(`/posts/${post.urlSlug}`);
-    console.log("Redirection done");
-  } catch (e) {
-    /* If a required information is missing */
-    /* Stay on the same page*/
-    console.log(e);
-    res.render("posts/new", {
-      post: post /* The fields will be prepopulated with the failed post. */
-    });
-  }
-});
+/* A function for editing posts: */
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.post = await Post.findById(req.params.id);
+    next();
+  },
+  savePost("edit")
+);
 
 /* A function for deleting posts:
 Because forms can only either GET or POST, we need to require method-override */
@@ -59,5 +61,34 @@ router.delete("/:id", async (req, res) => {
   await Post.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
+
+/* This function is called when creating a new or editing an existing
+post. It takes in a path which is either new or edit. That will be
+used in the redirection in an error case. */
+function savePost(path) {
+  return async (req, res) => {
+    /* Fetch the information of the post from the _formfields
+    and save them in database */
+    let post = req.post;
+    post.title = req.body.title;
+    post.content = req.body.content;
+
+    try {
+      /* If there's a success: */
+      /* Wait till the post is saved in database and then redirect to page /posts/id */
+      post = await post.save();
+      console.log("Inserted 1 post");
+      res.redirect(`/posts/${post.urlSlug}`);
+      console.log("Redirection done");
+    } catch (e) {
+      /* If a required information is missing */
+      /* Stay on the same page*/
+      console.log(e);
+      res.render("posts/${path}", {
+        post: post /* The fields will be prepopulated with the failed post. */
+      });
+    }
+  };
+}
 
 module.exports = router;
